50056f519384675af883a5e08dc46ff63a195b46
LuisPiriz <manupirizcorrea@gmail.com>
2023-01-16 21:06:10 +0000

    v1 navbar y cards

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,16 +2,35 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar bg-light">
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          <img
+            src="https://i.pinimg.com/originals/b4/b5/fd/b4b5fdf7bf06601ad4bd1cc6f73acff3.png"
+            alt="Bootstrap"
+            width="60"
+            height="60"
+          />
+        </a>
+        <div className="dropdown">
+          <button
+            className="btn btn-warning dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Favorites
+          </button>
+          <ul className="dropdown-menu">
+            <li>
+              <a className="dropdown-item" href="#">
+                Action
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
---
+++ src/js/component/planets.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { Card } from "./card";
+
+export const Planets = () => {
+  return (
+    <>
+    <div>
+        <h1>Planets</h1>
+    </div>
+    <div className="row row-cols-1 row-cols-md-2 g-4">
+        {/* <Card imageUrl="https://i.blogs.es/1da08b/1366_2000-9-/1366_2000.jpeg" title="Luke Skywalker" gender="male" hair="brown" eye="blue"/> */}
+        <Card/>
+    </div>
+    </>
+  )
+}
